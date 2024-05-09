d46060b9ddc307d7da6f8fb9a9988037fc3dd157
Om Prakash Mallik <opm202000@gmail.com>
2024-05-09 22:28:53 +0530

    nav completed responsively

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,7 +8,7 @@ function Nav() {
   const [showMenuItem, setShowMenuItem] = useState(false);
 
   return (
-    <div className='flex fixed top-0 opacity-80 w-full justify-between p-3 lg:px-36 lg:py-5 bg-white'>
+    <div className='flex fixed top-0 opacity-70 w-full justify-between p-3 lg:px-36 lg:py-5 bg-white'>
 
       <div>
         <h1 className="font-black text-4xl">LOGO</h1>
@@ -17,30 +17,30 @@ function Nav() {
       <div className="bg-white">
 
         {/* For small screens, show/hide navigation links */}
-        <div className="lg:hidden flex flex-col gap-8 items-end">
+        <div className="lg:hidden flex flex-col gap-5 items-end">
 
-          <Button variant="contained" onClick={() => setShowMenuItem(!showMenuItem)}>
-            {showMenuItem ? <CloseIcon /> : <MenuIcon />}
+          <Button variant="text" className="text-black" onClick={() => setShowMenuItem(!showMenuItem)}>
+            {showMenuItem ? <CloseIcon fontSize="large" /> : <MenuIcon fontSize="large" />}
           </Button>
 
           {showMenuItem && (
-            <div className="flex flex-col items-end gap-8">
+            <div className="flex flex-col items-end text-xl font-bold gap-8 pr-4 pb-5">
               <Link className="">HOME</Link>
               <Link className="">ABOUT</Link>
               <Link className="">TOUR PACKAGES</Link>
               <Link className="">CONTACT</Link>
-              <Button variant="contained" className="">Login</Button>
+              <Button variant="contained" size="large" className="">Login</Button>
             </div>
           )}
         </div>
 
         {/* For large screens, display navigation links */}
-        <div className="hidden lg:flex justify-end items-center text-md font-semibold gap-10">
+        <div className="hidden lg:flex justify-end items-center text-lg font-semibold gap-10">
           <Link>HOME</Link>
           <Link>ABOUT</Link>
           <Link>TOUR PACKAGES</Link>
           <Link>CONTACT</Link>
-          <Button variant="contained">Login</Button>
+          <Button variant="contained" size="large">Login</Button>
         </div>
       </div>
     </div>
