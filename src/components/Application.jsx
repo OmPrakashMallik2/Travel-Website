af1dd62c5f52fa1780de371cf7cf6cfcbc934cdd
Om Prakash Mallik <opm202000@gmail.com>
2024-05-10 18:57:02 +0530

    added login and signup components

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import Home from './Home'
-import Place from './Place'
 import Nav from './Nav'
 import Footer from './Footer'
+import { Route, Routes } from 'react-router-dom'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
+import TourPackages from './TourPackages'
 
 function Application() {
     return (
         <div>
             <Nav />
-            <Home />
-            <Place />
+            <Routes>
+                <Route path='/' element={< Home />} />
+                <Route path='/login' element={< SignIn />} />
+                <Route path='/signup' element={< SignUp />} />
+                <Route path='/packages' element={< TourPackages />} />
+            </Routes>
             <Footer />
         </div>
     )
